d3391527521e366b6bd40c1bbcd1d5c7e12b675d
ngani faith <nganifaith@gmail.com>
2021-04-05 13:58:23 +0100

    Add more test for event model and update Gemfile

--- Gemfile
+++ Gemfile
@@ -5,6 +5,10 @@ ruby '2.7.2'
 
 gem 'devise'
 
+gem 'rexml'
+
+gem 'shoulda-matchers'
+gem 'hirb'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.3', '>= 6.1.3.1'
 # Use sqlite3 as the database for Active Record
--- spec/models/events_spec.rb
+++ spec/models/events_spec.rb
@@ -11,4 +11,29 @@ RSpec.describe Event, type: :model do
   it 'ensures description presence' do
     expect(subject).to be_invalid
   end
+
+  context 'description validation' do
+    it 'ensures description presence' do
+      u = User.new(email: 'ngani@gmail.com')
+      u.save
+      expect(Event.new(description: 'Angular Clan', date: '1/1/2020', creator_id: 1)).to be_invalid
+    end
+  end
+  context 'description validation' do
+    it 'ensures description presence' do
+      expect(Event.new).to be_invalid
+    end
+  end
+
+  context 'description validation' do
+    it 'ensures description presence' do
+      expect(Event.new(description: 'star wars')).to be_invalid
+    end
+  end
+
+  describe 'Associations' do
+    it { should belong_to(:creator) }
+    it { should have_many(:attendees) }
+    it { should have_many(:invitations) }
+  end
 end
